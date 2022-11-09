8fa137eb4e57ca261bcb16dcf63b2af845409649
Carolina Hangai <108416398+carolhangai@users.noreply.github.com>
2022-11-09 09:45:47 -0300

    Conversor base numerica
    atualização

--- Exercicio037.py
+++ Exercicio037.py
@@ -1,6 +1,7 @@
 """Exercício Python 37: Escreva um programa em Python que leia um número inteiro qualquer e peça para
  o usuário escolher qual será a base de conversão: 1 para binário, 2 para octal e 3 para hexadecimal."""
 
+
 num = int(input('Digite um numero inteiro:'))
 print('O numero digitado foi:', num)
 print('Digite [1] para converter o numero em BINARIO.')
