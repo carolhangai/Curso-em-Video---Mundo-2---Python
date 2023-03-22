005f219a5eca0fe257de04131e1749a7b79de3bb
Carolina Hangai <carolina.hangai@gmail.com>
2023-03-22 15:24:55 -0300

    Contagem regressiva

--- Exercicio046.py
+++ Exercicio046.py
@@ -1,7 +1,12 @@
 """Exercício Python 46: Faça um programa que mostre na tela uma contagem regressiva para o estouro de fogos de artifício, indo de 10 até 0, com uma pausa de 1 segundo entre eles."""
+
 from time import sleep
-for c in range (10,0, -1):
+for c in range (10,-1, -1):
     sleep(0.5)
     print (c)
     sleep(0.5)
 print('Fogos de artificio explodem!🎆🎇')
+
+
+
+
